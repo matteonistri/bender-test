f539b472e164f79e90c453cb83f2a2df0261e370
Daniele Basile <asterix@develer.com>
2016-07-20 11:22:23 +0200

    Print os info on startup.

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/matishsiao/goInfo"
 )
 
 func main() {
+	gi := goInfo.GetInfo()
 	LogAppendLine(fmt.Sprintf("== Bender test =="))
+	LogAppendLine(fmt.Sprintf("Run on: %v", gi))
 	LogAppendLine(fmt.Sprintf("START  %s", time.Now()))
 
 	// Put init here..
