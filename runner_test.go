f5afbf45580c6e3dd2df7b4d6898cd9ae779024b
Daniele Basile <asterix24@gmail.com>
2016-07-31 19:37:15 +0200

    Add more tests.

--- runner_test.go
+++ runner_test.go
@@ -6,33 +6,32 @@ import (
 	"time"
 )
 
-func runner(name string) {
+func runner(name string) string {
 	job := &Job{}
 	ret := job.Run(name, "f3628fa3-2b25-4dfd-ac2d-2c8a8613915c", []string{})
+	if ret < 0 {
+		return JobNotFound
+	}
 	fmt.Println(ret)
 	logChannel := *job.Log()
 	stateChannel := *job.State()
 
-	exit := true
 	previousState := ""
-	for exit {
+	for {
 		select {
 		case m := <-logChannel:
-			//rep.UpdateString(m)
 			fmt.Println(m)
 		case s := <-stateChannel:
 			if previousState != s {
-				LogDeb(logContextTestRunner, "received state [%v]", s)
+				LogDeb(logContextTestRunner, "Receive [%v] state [%v]", name, s)
 				previousState = s
 			}
-			if s == JobCompleted || s == JobFailed {
-				exit = false
+			if s != JobWorking {
+				return s
 			}
-			break
 		case <-time.After(60 * time.Second):
-			LogDeb(logContextTestRunner, "Timeout!")
-			exit = false
-			break
+			LogDeb(logContextTestRunner, "Exec script [%v] Timeout!", name)
+			return JobTimeout
 		}
 	}
 }
@@ -47,11 +46,30 @@ func TestRunnerF(t *testing.T) {
 		name:  "RUNNER_TEST",
 		level: cm.GetLogLevel("general", 3)}
 
-	fmt.Print("\n\nUno Test..\n\n")
-	runner("uno")
-	fmt.Print("\n\nDue Test..\n\n")
-	runner("due")
-	fmt.Print("\n\nTre Test..\n\n")
-	runner("tre")
+	LogInf(logContextTestRunner, "Unknow Test..")
+	ret := runner("Unknow")
+	if ret != JobNotFound {
+		t.Errorf("Unknow should not found [%v].", ret)
+	}
+	LogInf(logContextTestRunner, "Foo Test..")
+	ret = runner("foo")
+	if ret != JobFailed {
+		t.Errorf("Foo script should fail [%v].", ret)
+	}
+	LogInf(logContextTestRunner, "Uno Test..")
+	ret = runner("uno")
+	if ret != JobFailed {
+		t.Errorf("Uno script is not FAIL [%v].", ret)
+	}
+	LogInf(logContextTestRunner, "Due Test..")
+	ret = runner("due")
+	if ret != JobFailed {
+		t.Errorf("Due script is not FAIL [%v].", ret)
+	}
+	LogInf(logContextTestRunner, "Tre Test..")
+	ret = runner("tre")
+	if ret != JobFailed {
+		t.Errorf("Tre script is not FAIL [%v].", ret)
+	}
 
 }
